6dd56ff404896e441c6401e2ca4cbc167fcf0f2c
iborzenko <iborzenko@baltbet.ru>
2021-06-02 12:10:24 +0300

    add package

--- PaymentDispatcher.DataContracts/Properties/AssemblyInfo.cs
+++ PaymentDispatcher.DataContracts/Properties/AssemblyInfo.cs
@@ -5,11 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PaymentDispatcher.Contracts")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PaymentDispatcher.Contracts")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +16,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("20e6ece6-137e-4a4b-805d-1620b9d88ad3")]
+[assembly: Guid("f69eb70e-418a-44f9-a8f0-0b790a8ca873")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +28,3 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
